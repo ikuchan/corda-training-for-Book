a51d11402a5f382d2a1db5abfd5d784949c77780
ikuchan <storyseller1980@gmail.com>
2019-05-05 23:33:37 +0900

    Contractテスト完了（JAVA)

--- java-source/src/main/java/net/corda/training/contract/IOUContract.java
+++ java-source/src/main/java/net/corda/training/contract/IOUContract.java
@@ -6,46 +6,78 @@ import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;
 import static net.corda.core.contracts.ContractsDSL.requireThat;
 
 import net.corda.core.identity.AbstractParty;
-import net.corda.core.identity.Party;
 import net.corda.core.transactions.LedgerTransaction;
 
-import net.corda.finance.contracts.asset.Cash;
 import net.corda.training.state.IOUState;
 
-import javax.swing.plaf.nimbus.State;
-import javax.validation.constraints.NotNull;
-import java.lang.reflect.Array;
 import java.security.PublicKey;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
- * This is where you'll add the contract code which defines how the [IOUState] behaves. Looks at the unit tests in
- * [IOUContractTests] for instructions on how to complete the [IOUContract] class.
+ * This is the contract code which defines how the [IOUState] behaves. Looks at the unit tests in
+ * [IOUContractTests] for more insight on how this contract verifies a transaction.
  */
+
+// LegalProseReference: this is just a dummy string for the time being.
+
+//@LegalProseReference(uri = "<prose_contract_uri>")
 public class IOUContract implements Contract {
     public static final String IOU_CONTRACT_ID = "net.corda.training.contract.IOUContract";
 
     /**
-     * Add any commands required for this contract as classes within this interface.
-     * It is useful to encapsulate your commands inside an interface, so you can use the [requireSingleCommand]
-     * function to check for a number of commands which implement this interface.
+     * The IOUContract can handle three transaction types involving [IOUState]s.
+     * - Issuance: Issuing a new [IOUState] on the ledger, which is a bilateral agreement between two parties.
+     * - Transfer: Re-assigning the lender/beneficiary.
+     * - Settle: Fully or partially settling the [IOUState] using the Corda [Cash] contract.
      */
     public interface Commands extends CommandData {
-        // Add commands here.
-        // E.g
-        // class DoSomething extends TypeOnlyCommandData implements Commands{}
+        class Issue extends TypeOnlyCommandData implements Commands{}
+        class Transfer extends TypeOnlyCommandData implements Commands{}
+        class Settle extends TypeOnlyCommandData implements Commands{}
     }
-
     /**
      * The contract code for the [IOUContract].
      * The constraints are self documenting so don't require any additional explanation.
      */
     @Override
     public void verify(LedgerTransaction tx) {
-        // Add contract code here.
-        // requireThat(req -> {
-        //     ...
-        // });
+
+        // We can use the requireSingleCommand function to extract command data from transaction.
+        final CommandWithParties<Commands> command = requireSingleCommand(tx.getCommands(), Commands.class);
+        final Commands commandData = command.getValue();
+
+        /**
+         * This command data can then be used inside of a conditional statement to indicate which set of tests we
+         * should be performing - we will use different assertions to enable the contract to verify the transaction
+         * for issuing, settling and transferring.
+         */
+        if (commandData.equals(new Commands.Issue())) {
+
+            requireThat(require -> {
+
+                require.using("No inputs should be consumed when issuing an IOU.", tx.getInputStates().size() == 0);
+                require.using( "Only one output state should be created when issuing an IOU.", tx.getOutputStates().size() == 1);
+
+                IOUState outputState = tx.outputsOfType(IOUState.class).get(0);
+                require.using( "A newly issued IOU must have a positive amount.", outputState.amount.getQuantity() > 0);
+                require.using( "The lender and borrower cannot have the same identity.", outputState.lender.getOwningKey() != outputState.borrower.getOwningKey());
+
+                List<PublicKey> signers = tx.getCommands().get(0).getSigners();
+                HashSet<PublicKey> signersSet = new HashSet<>();
+                for (PublicKey key: signers) {
+                    signersSet.add(key);
+                }
+
+                List<AbstractParty> participants = tx.getOutputStates().get(0).getParticipants();
+                HashSet<PublicKey> participantKeys = new HashSet<>();
+                for (AbstractParty party: participants) {
+                    participantKeys.add(party.getOwningKey());
+                }
+
+                require.using("Both lender and borrower together only may sign IOU issue transaction.", signersSet.containsAll(participantKeys) && signersSet.size() == 2);
+                return null;
+            });
+        }
     }
+
 }
--- java-source/src/main/java/net/corda/training/state/IOUState.java
+++ java-source/src/main/java/net/corda/training/state/IOUState.java
@@ -9,23 +9,30 @@ import com.google.common.collect.ImmutableList;
 import net.corda.core.serialization.ConstructorForDeserialization;
 import net.corda.training.contract.IOUContract;
 
-import javax.validation.constraints.NotNull;
-
 /**
- * This is where you'll add the definition of your state object. Look at the unit tests in [IOUStateTests] for
- * instructions on how to complete the [IOUState] class.
- *
+ * The IOU State object, with the following properties:
+ * - [amount] The amount owed by the [borrower] to the [lender]
+ * - [lender] The lending party.
+ * - [borrower] The borrowing party.
+ * - [contract] Holds a reference to the [IOUContract]
+ * - [paid] Records how much of the [amount] has been paid.
+ * - [linearId] A unique id shared by all LinearState states representing the same agreement throughout history within
+ *   the vaults of all parties. Verify methods should check that one input and one output share the id in a transaction,
+ *   except at issuance/termination.
  */
+
 @BelongsToContract(IOUContract.class)
-public class IOUState implements LinearState {
-    private final Amount<Currency> amount;
-    private final Party lender;
-    private final Party borrower;
-    private final Amount<Currency> paid;
+public class IOUState implements ContractState, LinearState {
+
+    public final Amount<Currency> amount;
+    public final Party lender;
+    public final Party borrower;
+    public final Amount<Currency> paid;
     private final UniqueIdentifier linearId;
 
+    // Private constructor used only for copying a State object
     @ConstructorForDeserialization
-    private IOUState(Amount<Currency> amount, Party lender, Party borrower, Amount<Currency> paid, UniqueIdentifier linearId) {
+    private IOUState(Amount<Currency> amount, Party lender, Party borrower, Amount<Currency> paid, UniqueIdentifier linearId){
         this.amount = amount;
         this.lender = lender;
         this.borrower = borrower;
@@ -34,37 +41,56 @@ public class IOUState implements LinearState {
     }
 
     public IOUState(Amount<Currency> amount, Party lender, Party borrower) {
-        this.amount = amount;
-        this.lender = lender;
-        this.borrower = borrower;
-        this.paid = new Amount<>(0,amount.getToken());
-        this.linearId = new UniqueIdentifier();
+        this(amount, lender, borrower, new Amount<>(0, amount.getToken()), new UniqueIdentifier());
     }
+
     public Amount<Currency> getAmount() {
         return amount;
     }
+
     public Party getLender() {
         return lender;
     }
+
     public Party getBorrower() {
         return borrower;
     }
+
     public Amount<Currency> getPaid() {
         return paid;
     }
+
+    @Override
+    public UniqueIdentifier getLinearId() {
+        return linearId;
+    }
+
+    /**
+     *  This method will return a list of the nodes which can "use" this state in a valid transaction. In this case, the
+     *  lender or the borrower.
+     */
     @Override
     public List<AbstractParty> getParticipants() {
-        return ImmutableList.of(lender,borrower);
+        return ImmutableList.of(lender, borrower);
     }
-    @Override
-    public UniqueIdentifier getLinearId(){
-        return getLinearId();
-    }
-    public IOUState pay(Amount<Currency>amountToPay){
+
+    /**
+     * Helper methods for when building transactions for settling and transferring IOUs.
+     * - [pay] adds an amount to the paid property. It does no validation.
+     * - [withNewLender] creates a copy of the current state with a newly specified lender. For use when transferring.
+     * - [copy] creates a copy of the state using the internal copy constructor ensuring the LinearId is preserved.
+     */
+    public IOUState pay(Amount<Currency> amountToPay) {
         Amount<Currency> newAmountPaid = this.paid.plus(amountToPay);
         return new IOUState(amount, lender, borrower, newAmountPaid, linearId);
     }
+
     public IOUState withNewLender(Party newLender) {
         return new IOUState(amount, newLender, borrower, paid, linearId);
     }
+
+    public IOUState copy(Amount<Currency> amount, Party lender, Party borrower, Amount<Currency> paid) {
+        return new IOUState(amount, lender, borrower, paid, this.getLinearId());
+    }
+
 }
